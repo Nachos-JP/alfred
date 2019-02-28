317ba8e9b3668ffd0c6d67942da920f13746f9b5
Shota Maekawa <f22raptor0817@gmail.com>
2019-02-28 22:40:19 +0900

    [update] To draggable & set respawn window size and coord

--- src/main.js
+++ src/main.js
@@ -2,16 +2,30 @@ const electron = require("electron");
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
+const Store = require("electron-store");
+const store = new Store();
+
 let mainWindow = null;
 
 const createWindow = ()=>{
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: store.get("window.bound.width")?store.get("window.bound.width"):800,
+    height: store.get("window.bound.height")?store.get("window.bound.height"):600,
+    x: store.get("window.bound.x")?store.get("window.bound.x"):null,
+    y: store.get("window.bound.y")?store.get("window.bound.y"):null,
     frame: false
   });
   mainWindow.loadFile("index.html");
   mainWindow.webContents.openDevTools();
+  mainWindow.on("close", ()=>{
+    const bound = mainWindow.getBounds();
+    store.set("window.bound", {
+      width: bound.width,
+      height: bound.height,
+      x: bound.x,
+      y: bound.y
+    });
+  })
   mainWindow.on("closed", ()=>{
     mainWindow = null;
   })
