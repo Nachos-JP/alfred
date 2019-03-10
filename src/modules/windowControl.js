468a0cbb7ee9fafbb6b0cb2814827fa0329253d6
Shota Maekawa <f22raptor0817@gmail.com>
2019-03-10 22:31:40 +0900

    [update] Add window maximize or unmaximize toggle func

--- src/modules/windowControl.js
+++ src/modules/windowControl.js
@@ -1,6 +1,12 @@
 const remote = require("electron").remote;
 
 module.exports = {
+  toggleWindowBtn: function(){
+    const windowMaxBtn = document.getElementById("window-max-btn");
+    const windowRestoreBtn = document.getElementById("window-restore-btn");
+    windowMaxBtn.classList.toggle("show");
+    windowRestoreBtn.classList.toggle("show");
+  },
   minimize: function(){
     const window = remote.getCurrentWindow();
     window.minimize();
@@ -8,10 +14,12 @@ module.exports = {
   maximize: function(){
     const window = remote.getCurrentWindow();
     window.maximize();
+    this.toggleWindowBtn();
   },
   restore: function(){
     const window = remote.getCurrentWindow();
     window.unmaximize();
+    this.toggleWindowBtn();
   },
   close: function(){
     const window = remote.getCurrentWindow();
